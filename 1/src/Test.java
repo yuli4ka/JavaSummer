93d3d67aa73b93822ec07910d0432bd542d914ea
yuli4ka <yuli4kazhitaryuk@gmail.com>
2019-07-12 01:57:47 +0300

    check if .gitignore works

---
+++ 1/src/Test.java
@@ -0,0 +1,54 @@
+//public class Test {
+//  public static void main(String[] args){
+//    char c2 = '\u0031';
+//    char c1 = '1'; // '\u0031';
+//    char c3 = 49; // '\u0031';
+//    System.out.println(c1 + c2 + c3);
+//  }
+//}
+
+//public class Test {
+//  public static void main(String[] args){
+//    char c2 = '\u0031';
+//    char c1 = '1'; // '\u0031';
+//    char c3 = 49; // '\u0031';
+//    System.out.println("" + c1 + c2 + c3);
+//  }
+//}
+
+//public class Test {
+//  public static void main(String[] args){
+//    int v1 = 1;
+//    long v2 = 2;
+////    v1 = v1 + v2;
+//     v1+=v2;
+//     System.out.println(v1);
+//  }
+//}
+
+//public class Test {
+//
+//  public static void main(String[] args) {
+//    byte b = 1;
+//    byte c = b + 1;
+//    System.out.println(c);
+//  }
+//}
+
+//public class Test {
+//
+//  public static void main(String[] args) {
+//    int x = 1;
+//    System.out.println("x=" + x);
+//    System.out.println(1 + 2 + "text");
+//    System.out.println("text" + 1 + 2);
+//  }
+//}
+
+public class Test {
+
+  public static void main(String[] args) {
+    int [] a;
+    int b[];
+  }
+}
