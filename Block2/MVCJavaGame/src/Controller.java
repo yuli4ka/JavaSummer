75c15ae8e927872527873b978466142cabc7be51
yuli4ka <yuli4kazhitaryuk@gmail.com>
2019-07-12 13:53:41 +0300

    create classes MVC

---
+++ Block2/MVCJavaGame/src/Controller.java
@@ -0,0 +1,14 @@
+class Controller {
+
+  private Model model;
+  private View view;
+
+  Controller(Model model, View view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  void doTask() {
+    
+  }
+}
