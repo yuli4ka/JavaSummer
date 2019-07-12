986ffadd0a03da7b3f8a353b99f33b184216bf66
yuli4ka <yuli4kazhitaryuk@gmail.com>
2019-07-12 14:20:21 +0300

    add view and model methods

--- Block2/MVCJavaGame/src/Model.java
+++ Block2/MVCJavaGame/src/Model.java
@@ -1,3 +1,21 @@
 class Model {
 
+  private int hiddenNumber;
+
+  Model() {
+    hiddenNumber = (int)(Math.random() * 100);
+  }
+
+  boolean isHiddenNumber(int number) {
+    return number == hiddenNumber;
+  }
+
+  boolean lessThanHiddenNumber(int number) {
+    return number < hiddenNumber;
+  }
+
+  boolean greaterThanHiddenNumber(int number) {
+    return number > hiddenNumber;
+  }
+
 }
--- Block2/MVCJavaGame/src/View.java
+++ Block2/MVCJavaGame/src/View.java
@@ -1,3 +1,22 @@
 class View {
 
+  public static final String INPUT_NUMBER_BETWEEN = "Input number between ";
+  public static final String AND = " and ";
+  public static final String YOU_ENTERED_NOT_A_NUMBER = "You entered not a number. ";
+  public static final String YOUR_NUMBER_IS_OUT_OF_RANGE = "Your number is out of range. ";
+  public static final String HIDDEN_NUMBER_LESS_THAN = "Hidden number less than ";
+  public static final String HIDDEN_NUMBER_GREATER_THAN = "Hidden number greater than ";
+
+  void printMessage(String message) {
+    System.out.println(message);
+  }
+
+  void pringRange(int low, int high) {
+    System.out.println(INPUT_NUMBER_BETWEEN + low + AND + high);
+  }
+
+  void printMessagePlusInt(String message, int number) {
+    System.out.println(message + number);
+  }
+
 }
