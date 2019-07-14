ebec51d6150829a11335859ec2cd42f86aad616a
yuli4ka <yuli4kazhitaryuk@gmail.com>
2019-07-15 00:09:01 +0300

    create class Arithmetics

---
+++ Block2/tryJUnitByVideo/src/calculation/Arithmetics.java
@@ -0,0 +1,20 @@
+package calculation;
+
+public class Arithmetics {
+
+  public double add(double a, double b) {
+    return a + b;
+  }
+
+  public double deduct(double a, double b) {
+    return a - b;
+  }
+
+  public double mult(double a, double b) {
+    return a * b;
+  }
+
+  public double divide(double a, double b) {
+    return a / b;
+  }
+}
