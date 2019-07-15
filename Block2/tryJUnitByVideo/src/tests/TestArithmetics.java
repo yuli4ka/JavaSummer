b1fa947d688ef93b7d970349597be495df231597
yuli4ka <yuli4kazhitaryuk@gmail.com>
2019-07-15 15:26:23 +0300

    add test rules for timeout and exceptions

--- Block2/tryJUnitByVideo/src/tests/TestArithmetics.java
+++ Block2/tryJUnitByVideo/src/tests/TestArithmetics.java
@@ -1,14 +1,22 @@
 package tests;
 
-
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import calculation.Arithmetics;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
 
 public class TestArithmetics {
+
   private static Arithmetics arithmetics;
+
+  @Rule
+  public final ExpectedException exp = ExpectedException.none();
+
+  @Rule
+  public Timeout time = new Timeout(1000);
 
   @BeforeClass
   public static void beforeClass() {
@@ -19,32 +27,44 @@ public class TestArithmetics {
   @Test
   public void add() {
     double res = arithmetics.add(3, 7);
-    if (res != 10) {
-      Assert.fail();
-    }
+    Assert.assertEquals(10.0, res, 0.0);
   }
 
   @Test
   public void deduct() {
     double res = arithmetics.deduct(3, 7);
-    if (res != -4) {
-      Assert.fail();
-    }
+    Assert.assertEquals(-4, res, 0.0);
   }
 
   @Test
   public void mult() {
     double res = arithmetics.mult(3, 7);
-    if (res != 21) {
-      Assert.fail();
-    }
+    Assert.assertEquals(21, res, 0.0);
   }
 
   @Test
   public void divide() {
     double res = arithmetics.divide(10, 5);
-    if (res != 2) {
-      Assert.fail();
-    }
+    Assert.assertEquals(2, res, 0.0);
+
+//    try {
+//      arithmetics.divide(10,0);
+//      Assert.fail();
+//    } catch (Exception e) {}
+
   }
+
+//  @Test(expected = ArithmeticException.class)
+  @Test
+  public void divideExeption() {
+    exp.expect(ArithmeticException.class);
+    arithmetics.divide(10.0, 0.0);
+  }
+
+//  @Test(timeout = 1000)
+  @Test
+  public void testN() {
+    while (true);
+  }
+
 }
