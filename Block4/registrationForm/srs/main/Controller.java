3629e424ea40d60f85cc7846a82c5e689d61dce5
yuli4ka <yuli4kazhitaryuk@gmail.com>
2019-07-17 14:39:24 +0300

    add input for nickname

--- Block4/registrationForm/srs/main/Controller.java
+++ Block4/registrationForm/srs/main/Controller.java
@@ -17,11 +17,13 @@ class Controller {
   }
 
   void startTask() {
-    model.setSurname(getNameForPattern(View.ENTER_YOUR_SURNAME));
-    model.setName(getNameForPattern(View.ENTER_YOUR_NAME));
-    model.setPatronymic(getNameForPattern(View.ENTER_YOUR_PATRONYMIC));
-    model.setInitialName();
-    view.printMessage(model.getInitialName());
+//    model.setSurname(getNameForPattern(View.ENTER_YOUR_SURNAME));
+//    model.setName(getNameForPattern(View.ENTER_YOUR_NAME));
+//    model.setPatronymic(getNameForPattern(View.ENTER_YOUR_PATRONYMIC));
+//    model.setInitialName();
+//    view.printMessage(model.getInitialName());
+
+    model.setNickname(getNicknameForPattern());
   }
 
   private String getNameForPattern(String message) {
@@ -35,12 +37,17 @@ class Controller {
     return name;
   }
 
-//  private String getNicknmeForPattern() {
-//    view.printMessage(View.ENTER_YOUR_NICKNAME);
-//    String nickname = in.nextLine();
-//    while (!patterns.getNicknamePattern().matcher(nickname).matches()) {
-//
-//    }
-//  }
+  private String getNicknameForPattern() {
+    view.printMessage(View.ENTER_YOUR_NICKNAME);
+    String nickname = in.nextLine();
+    while (!patterns.getNicknamePattern().matcher(nickname).matches()) {
+      view.printMessage(View.WRONG_INPUT);
+      view.printMessage(View.ENTER_YOUR_NICKNAME);
+      nickname = in.nextLine();
+    }
+    return nickname;
+  }
+
+
 
 }
